90ad8e75dd4d27269f9c75a02a308e9986d76e17
Hunter Mast <127757099+HunterCadeMast@users.noreply.github.com>
2026-02-12 15:45:11 -0500

    Create 2820-ReturnLengthOfArgumentsPassed

---
+++ 2820-ReturnLengthOfArgumentsPassed/2820-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,12 @@
+// Last updated: 2/12/2026, 3:45:13 PM
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
