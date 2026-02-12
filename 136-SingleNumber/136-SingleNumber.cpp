2c99f28873a5997a2fce41adf6871b0252ead37c
Hunter Mast <127757099+HunterCadeMast@users.noreply.github.com>
2026-02-12 15:45:29 -0500

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.cpp
@@ -0,0 +1,11 @@
+// Last updated: 2/12/2026, 3:45:31 PM
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+};
