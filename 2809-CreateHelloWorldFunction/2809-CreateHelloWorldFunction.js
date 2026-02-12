b2db63c97e19cc8114578f840eee91c74ff8692b
Hunter Mast <127757099+HunterCadeMast@users.noreply.github.com>
2026-02-12 15:45:12 -0500

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 2/12/2026, 3:45:15 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
