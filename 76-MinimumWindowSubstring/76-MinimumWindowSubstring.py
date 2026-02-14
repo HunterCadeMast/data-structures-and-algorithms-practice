452d064bb429d7729fa8556cffacc131b9eb0884
Hunter Mast <127757099+HunterCadeMast@users.noreply.github.com>
2026-02-13 19:34:51 -0500

    File updated at 2/13/2026, 7:34:51 PM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
@@ -1,4 +1,13 @@
-# Last updated: 2/13/2026, 7:30:40 PM
+# Last updated: 2/13/2026, 7:34:51 PM
 1class Solution:
 2    def findMin(self, nums: List[int]) -> int:
-3        return min(nums)
+3        left = 0
+4        right = len(nums) - 1
+5        while left < right:
+6            middle = left + (right - left) // 2
+7            if nums[middle] < nums[right]:
+8                right = middle
+9            else:
+10                left = middle + 1
+11        return nums[left]
+12
