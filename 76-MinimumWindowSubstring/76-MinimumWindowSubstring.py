bb1f1523aa86065ac62677e940120b6c7da208a3
Hunter Mast <127757099+HunterCadeMast@users.noreply.github.com>
2026-02-13 19:24:30 -0500

    File updated at 2/13/2026, 7:24:30 PM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
@@ -1,29 +1,23 @@
-# Last updated: 2/13/2026, 7:13:08 PM
+# Last updated: 2/13/2026, 7:24:30 PM
 1class Solution:
-2    def minWindow(self, s: str, t: str) -> str:
-3        if not s or not t:
-4            return ""
-5        count = {}
-6        for letter in t:
-7            count[letter] = count.get(letter, 0) + 1
-8        left = 0
-9        min_length = float("inf")
-10        result = (0, 0)
-11        missing = len(t)
-12        for right, letter in enumerate(s):
-13            if letter in count:
-14                if count[letter] > 0:
-15                    missing -= 1
-16                count[letter] -= 1
-17            while missing == 0:
-18                if right - left + 1 < min_length:
-19                    min_length = right - left + 1
-20                    result = (left, right)
-21                left_letter = s[left]
-22                if left_letter in count:
-23                    count[left_letter] += 1
-24                    if count[left_letter] > 0:
-25                        missing += 1
-26                left += 1
-27        l, r = result
-28        return s[l:r + 1] if min_length != float("inf") else ""
+2    def isValid(self, s: str) -> bool:
+3        stack = []
+4        for bracket in s:
+5            if bracket == '(' or bracket == '{' or bracket == '[':
+6                stack.append(bracket)
+7            elif bracket == ')' or bracket == '}' or bracket == ']':
+8                if len(stack) == 0:
+9                    return False
+10                if bracket == ')' and stack[-1] == '(':
+11                    stack.pop()
+12                elif bracket == '}' and stack[-1] == '{':
+13                    stack.pop()
+14                elif bracket == ']' and stack[-1] == '[':
+15                    stack.pop()
+16                else:
+17                    return False
+18        if len(stack) == 0:
+19            return True
+20        else:
+21            return False
+22
