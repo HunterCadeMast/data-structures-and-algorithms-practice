104079e49b12c0fa8ea66d79753c9cbe6ef2969f
Hunter Mast <127757099+HunterCadeMast@users.noreply.github.com>
2026-02-15 14:33:31 -0500

    File updated at 2/15/2026, 2:33:34 PM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.py
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.py
@@ -1,4 +1,7 @@
-# Last updated: 2/15/2026, 2:32:59 PM
+# Last updated: 2/15/2026, 2:33:34 PM
 1class Solution:
-2    def isAnagram(self, s: str, t: str) -> bool:
-3        return sorted(s) == sorted(t)
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        for i in range(len(nums)):
+4            for j in range(i + 1, len(nums)):
+5                if nums[i] + nums[j] == target:
+6                    return [i, j]
