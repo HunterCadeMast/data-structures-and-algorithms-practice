5fe7b667106023cc5bb1c0c4012e15292cf92754
Hunter Mast <127757099+HunterCadeMast@users.noreply.github.com>
2026-02-16 16:49:25 -0500

    File updated at 2/16/2026, 4:49:26 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -1,16 +1,19 @@
-# Last updated: 2/16/2026, 4:21:03 PM
+# Last updated: 2/16/2026, 4:49:26 PM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, val=0, next=None):
 4#         self.val = val
 5#         self.next = next
 6class Solution:
-7    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-8        current = head
-9        previous = None
-10        while current is not None:
-11            next_node = current.next
-12            current.next = previous
-13            previous = current
-14            current = next_node
-15        return previous
+7    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+8        temp = node = ListNode()
+9        while list1 and list2:
+10            if list1.val >= list2.val:
+11                node.next = list2
+12                list2 = list2.next
+13            else:
+14                node.next = list1
+15                list1 = list1.next
+16            node = node.next
+17        node.next = list1 or list2
+18        return temp.next
