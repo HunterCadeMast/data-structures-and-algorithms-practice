3f6b12d52b0be686c996013c6dd118dbab30f84c
Hunter Mast <127757099+HunterCadeMast@users.noreply.github.com>
2026-02-18 17:47:32 -0500

    File updated at 2/18/2026, 5:47:32 PM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.py
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.py
@@ -1,14 +1,21 @@
-# Last updated: 2/18/2026, 5:22:15 PM
+# Last updated: 2/18/2026, 5:47:32 PM
 1class Solution:
-2    def maxArea(self, heights: List[int]) -> int:
-3        left = 0
-4        right = len(heights) - 1
-5        max_area = 0
-6        while left < right:
-7            area = min(heights[left], heights[right]) * (right - left)
-8            max_area = max(max_area, area)
-9            if heights[left] <= heights[right]:
-10                left += 1
-11            else:
-12                right -= 1
-13        return max_area
+2    def productExceptSelf(self, nums: List[int]) -> List[int]:
+3        results = []
+4        total_product = 1
+5        zero_check = nums.count(0)
+6        if zero_check >= 2:
+7            for i in range(len(nums)):
+8                results.append(0)
+9            return results
+10        for num in nums:
+11            if num != 1 and num != 0:
+12                total_product = total_product * num
+13        for i in range(len(nums)):
+14            if nums[i] == 0:
+15                results.append(total_product)
+16            elif nums[i] != 0 and zero_check > 0:
+17                results.append(0)
+18            else:
+19                results.append(total_product // nums[i])
+20        return results
