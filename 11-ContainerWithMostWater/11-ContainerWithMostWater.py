e189e14029d2551b1be9a38a09ad28b62f14c0a2
Hunter Mast <127757099+HunterCadeMast@users.noreply.github.com>
2026-02-18 22:14:31 -0500

    File updated at 2/18/2026, 10:14:31 PM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.py
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.py
@@ -1,21 +1,18 @@
-# Last updated: 2/18/2026, 5:47:32 PM
+# Last updated: 2/18/2026, 10:14:31 PM
 1class Solution:
-2    def productExceptSelf(self, nums: List[int]) -> List[int]:
-3        results = []
-4        total_product = 1
-5        zero_check = nums.count(0)
-6        if zero_check >= 2:
-7            for i in range(len(nums)):
-8                results.append(0)
-9            return results
-10        for num in nums:
-11            if num != 1 and num != 0:
-12                total_product = total_product * num
-13        for i in range(len(nums)):
-14            if nums[i] == 0:
-15                results.append(total_product)
-16            elif nums[i] != 0 and zero_check > 0:
-17                results.append(0)
-18            else:
-19                results.append(total_product // nums[i])
-20        return results
+2    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+3        groups = []
+4        group_sorted = []
+5        for word in strs:
+6            sorted_word = sorted(word)
+7            if len(groups) == 0:
+8                groups.append([word])
+9                group_sorted.append(sorted_word)
+10            else:
+11                if sorted_word in group_sorted:
+12                    group_index = group_sorted.index(sorted_word)
+13                    groups[group_index].append(word)
+14                else:
+15                    groups.append([word])
+16                    group_sorted.append(sorted_word)
+17        return groups
